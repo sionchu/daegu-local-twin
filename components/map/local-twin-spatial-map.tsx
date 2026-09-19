1606962b706e9ba39747926992f3d2858c7a520e
sionchu <140263362+sionchu@users.noreply.github.com>
2026-09-20 00:23:57 +0900

    remove fallback solar-time plumbing

--- components/map/local-twin-spatial-map.tsx
+++ components/map/local-twin-spatial-map.tsx
@@ -6,7 +6,6 @@ import type { LocationEvidence, MapLayer } from "@/src/types";
 import VWorldLocalTwinMap from "@/components/map/vworld-local-twin-map";
 
 const MapLibreFallback = lazy(() => import("@/components/map/local-twin-map"));
-const FALLBACK_LIGHT_TIME = new Date("2026-09-19T18:00:00+09:00");
 
 type Props = {
   cells: LocationEvidence[];
@@ -36,7 +35,6 @@ export default function LocalTwinSpatialMap(props: Props) {
             cells={props.cells}
             selectedCellId={props.selectedCellId}
             activeLayer={props.activeLayer}
-            selectedTime={FALLBACK_LIGHT_TIME}
             showTerrain
             showBuildings
             showExtrusion
